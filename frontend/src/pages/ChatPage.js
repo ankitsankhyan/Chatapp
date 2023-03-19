45d8b40aad51b8bbfae02b3c48ddc27456abd930
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-03-19 18:57:31 +0530

    chat model + messagemodel

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 const ChatPage = () => {
+    const [chat,setchat] = useState([]);
     const fetchChats = async () =>{
         const data = await axios.get("/api/chat");
-        console.log(data);
-        
+       
+        setchat(data.data);
+
+      
+       
     }
+
+    console.log(chat, 'chat');
     useEffect(()=>{
         console.log('running')
         fetchChats();
     },[])
   return (
-    <div>ChatPage</div>
+    <div>ChatPage
+       {chat.map((chat)=>{return <div key={chat._id}>
+                 {chat.chatName}
+                 </div>})}
+    </div>
   )
 }
 
