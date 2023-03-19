2d3e745d6a3ce5fa04584ff206efeaa3627ca2d2
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-03-19 11:55:14 +0530

    setup chat app

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chats = require('./data/data').chats;
+const app = express();
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+app.listen(PORT, function(err){
+    console.log('server started');
+})
+
+app.get('/', (req,res)=>{
+    res.send('App is running');
+})
+
+app.get('/chats',(req,res)=>{
+    res.send(chats);
+})
