5ad8b4ed8dd7988cb56470e883430dec071a9c5a
Ankit Sankhyan <ankitsankhyan04@gmail.com>
2023-04-01 01:51:39 +0530

    adding profileModal

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { Box, Button, Tooltip, Text } from "@chakra-ui/react";
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+  } from '@chakra-ui/react'
+import {BellIcon, ChevronDownIcon} from '@chakra-ui/icons'
+import {ChatState} from '../../context/chatProvider';
+import ProfileModal from './ProfileModal'
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [Loading, setLoading] = useState(false);
   const [LoadingChat, setLoadingChat] = useState();
-
+  const user= ChatState();
+  console.log(ProfileModal);
   return (
     <>
+    
       <Box display="flex"
        justifyContent="space-between"
        alignItems="center"
@@ -26,6 +38,23 @@ const SideDrawer = () => {
         </Tooltip>
 
         <Text fontFamily="Work sans" as= 'b' fontSize='2xl'>Chat App</Text>
+<div>
+ <Menu>
+    <MenuButton p =  {1}>
+    <BellIcon boxSize={6} m = {1}></BellIcon>
+    </MenuButton>
+ </Menu>
+ <Menu>
+    {/* if name is absent then text is written */}
+ <MenuButton p =  {1}  as = {Button} rightIcon = {<ChevronDownIcon/>}> <Avatar size = 'sm' cursor= 'pointer' name="user.name" src = {user.pic}/></MenuButton>
+ <MenuList>
+ <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>{" "}
+              </ProfileModal>
+    <MenuItem>LogOut</MenuItem>
+ </MenuList>
+ </Menu>
+</div>
       </Box>
     </>
   );
